a0cf85d3066d23499cf5c292937c0220589d4778
ryoid <ryanjconceicao@gmail.com>
2021-04-07 23:08:42 +0800

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,53 @@
 import React from 'react'
 
-import { useMyHook } from 'react-filepicker-hook'
+import { useFilePicker } from 'react-filepicker-hook'
+
+const loadFile = (file) => new Promise((resolve, reject) => {
+  const reader = new FileReader()
+
+  reader.onload = (e) => {
+    resolve(e.target?.result?.toString?.())
+  }
+
+  reader.onerror = (e) => {
+    console.log(e)
+    reject(new Error('Failed to load file.'))
+  }
+
+  reader.readAsDataURL(file)
+})
 
 const App = () => {
-  const example = useMyHook()
+  const [images, setImages] = React.useState([])
+  const { files, showFilePicker, errors, FileInput } = useFilePicker({
+    minSize: 1000000000
+  })
+
+  React.useEffect(() => {
+    const loadFiles = async (_files) => {
+      console.log('files', _files);
+
+      if (_files.length > 0) {
+        const data = await Promise.all(_files.map(loadFile))
+        setImages(data)
+      }
+    }
+    loadFiles(files)
+  }, [files])
+
   return (
-    <div>
-      {example}
-    </div>
+    <>
+      <button onClick={showFilePicker}>Show FileInput</button>
+      <FileInput accept="image/*" multiple />
+      <div>
+        {errors.map(error => JSON.stringify(error))}
+      </div>
+      <div style={{ maxWidth: 250 }}>
+        {images.map((file, i) => (
+          <img src={file} key={i} alt={`file-${i}`} />
+        ))}
+      </div>
+    </>
   )
 }
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,62 @@
-import * as React from 'react';
+import React from 'react'
+export interface FileInputPickerOptions {
+  minSize?: number
+  maxSize?: number
+}
+export interface FileInputPickerError {
+  index: number;
+  message: string;
+}
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
+export interface FileInputProps extends Omit<React.HTMLProps<HTMLInputElement>,'ref' | 'onChange'> {}
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
-    };
-  }, []);
+export const useFilePicker = ({
+  minSize, 
+  maxSize,
+}: FileInputPickerOptions = {}) => {
+  const inputRef = React.useRef<HTMLInputElement>(null)
+  const [files, setFiles] = React.useState<File[]>([])
+  const [errors, setErrors] = React.useState<FileInputPickerError[]>([])
 
-  return counter;
-};
+  const _onChange = React.useCallback((evt: React.ChangeEvent<HTMLInputElement>): void => {
+    const fileList = evt?.target?.files
+
+    const newFiles = Array.prototype.slice.call(fileList)
+    
+    const newErrors: FileInputPickerError[] = []
+    newFiles.forEach((file, index) => {
+      if (minSize && minSize > file.size) newErrors.push({
+        index,
+        message: 'File smaller than minimum size.'
+      })
+      else if (maxSize && maxSize < file.size) newErrors.push({
+        index,
+        message: 'File larger than maximum size.'
+      })
+    })
+
+    setErrors(newErrors)
+    setFiles(newFiles)
+  }, [])
+
+  const showFilePicker = React.useCallback(() => {
+    inputRef?.current?.click()
+  }, [inputRef?.current])
+
+  return {
+    files,
+    errors,
+    showFilePicker,
+    FileInput(inputProps: FileInputProps): React.ReactElement {
+      return (
+        <input
+          type="file"
+          ref={inputRef}
+          style={{ display: 'none' }}
+          onChange={_onChange}
+          {...inputProps}
+        />
+      )
+    }
+  }
+}
